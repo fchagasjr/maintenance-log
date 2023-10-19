a55bd744ebc499545d0819941156b837f6e299fd
Francisco das Chagas Silva Junior <fcsjr.chagas@gmail.com>
2023-10-19 11:12:04 -0400

    Set display responsive style to revoke key button

--- public/js/keys-table.js
+++ public/js/keys-table.js
@@ -1,13 +1,18 @@
-let userKeyRows = document.querySelectorAll("[id^='user-key-']");
 
-userKeyRows.forEach((userKeyRow) => {
-  userKeyRow.addEventListener("click", function() {
-    let revokeButton = document.getElementById(`revoke-${userKeyRow.id}`);
-    if (revokeButton.style.display == "none") {
-      revokeButton.style.display = "block";
-    } else {
-      revokeButton.style.display = "none";
-    }
-    console.log("Clicked");
+window.addEventListener("load", function() {
+  let userKeyRows = document.querySelectorAll("[id^='userKey']");
+
+  userKeyRows.forEach((userKeyRow) => {
+    userKeyRow.addEventListener("click", function() {
+      let revokeButton = document.getElementById(`revoke-${userKeyRow.id}`);
+      if (revokeButton.style.display == "inline-block") {
+        revokeButton.style.display = "none";
+      } else {
+        revokeButton.style.display = "inline-block";
+      }
+    })
   })
+
 })
+
+
